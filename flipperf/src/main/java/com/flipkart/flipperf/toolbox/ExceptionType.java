516e6a45b3bea8db8fb64aaf28da1aca66dbcd3d
anirudh.r <anirudh.r@flipkart.com>
2016-05-13 19:09:12 +0530

    exception subtype

---
+++ flipperf/src/main/java/com/flipkart/flipperf/toolbox/ExceptionType.java
@@ -0,0 +1,45 @@
+package com.flipkart.flipperf.toolbox;
+
+import java.io.IOException;
+import java.net.HttpRetryException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.net.UnknownServiceException;
+
+/**
+ * Created by anirudh.r on 13/05/16 at 12:31 PM.
+ */
+public class ExceptionType {
+
+    private final static int SOCKET_EXCEPTION = 601;
+    private final static int PROTOCOL_EXCEPTION = 602;
+    private final static int UNKNOWN_HOST_EXCEPTION = 603;
+    private final static int UNKNOWN_SERVICE_EXCEPTION = 604;
+    private final static int SOCKET_TIMEOUT_EXCEPTION = 605;
+    private final static int MALFORMED_URL_EXCEPTION = 606;
+    private final static int HTTP_RETRY_EXCEPTION = 606;
+    private final static int UNKNOWN_EXCEPTION = -1;
+
+    public static int getExceptionType(IOException e) {
+        if (e instanceof SocketException) {
+            return SOCKET_EXCEPTION;
+        } else if (e instanceof ProtocolException) {
+            return PROTOCOL_EXCEPTION;
+        } else if (e instanceof UnknownHostException) {
+            return UNKNOWN_HOST_EXCEPTION;
+        } else if (e instanceof UnknownServiceException) {
+            return UNKNOWN_SERVICE_EXCEPTION;
+        } else if (e instanceof SocketTimeoutException) {
+            return SOCKET_TIMEOUT_EXCEPTION;
+        } else if (e instanceof MalformedURLException) {
+            return MALFORMED_URL_EXCEPTION;
+        } else if (e instanceof HttpRetryException) {
+            return HTTP_RETRY_EXCEPTION;
+        } else {
+            return UNKNOWN_EXCEPTION;
+        }
+    }
+}
