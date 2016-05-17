1cbcf6a4f92f61597e2ba744b513cfce090be3b4
anirudh.r <anirudh.r@flipkart.com>
2016-05-17 22:42:30 +0530

    constructor to take in request

--- flipperf/src/main/java/com/flipkart/flipperf/response/DefaultResponseHandler.java
+++ flipperf/src/main/java/com/flipkart/flipperf/response/DefaultResponseHandler.java
@@ -5,12 +5,14 @@ import com.flipkart.flipperf.NetworkEventReporter;
 
 public class DefaultResponseHandler implements ResponseHandler {
     private final NetworkEventReporter mEventReporter;
-    private NetworkEventReporter.InspectorResponse inspectorResponse;
+    private final NetworkEventReporter.InspectorResponse inspectorResponse;
+    private final NetworkEventReporter.InspectorRequest inspectorRequest;
     private int mBytesRead = 0;
 
-    public DefaultResponseHandler(NetworkEventReporter eventReporter, NetworkEventReporter.InspectorResponse inspectorResponse) {
+    public DefaultResponseHandler(NetworkEventReporter eventReporter, NetworkEventReporter.InspectorRequest inspectorRequest, NetworkEventReporter.InspectorResponse inspectorResponse) {
         this.mEventReporter = eventReporter;
         this.inspectorResponse = inspectorResponse;
+        this.inspectorRequest = inspectorRequest;
     }
 
     @Override
@@ -20,6 +22,6 @@ public class DefaultResponseHandler implements ResponseHandler {
 
     @Override
     public void onEOF() {
-        mEventReporter.responseDataReceived(inspectorResponse, mBytesRead);
+        mEventReporter.responseDataReceived(inspectorRequest, inspectorResponse, mBytesRead);
     }
 }
