3e2e635370e80f80c8a87c447efa59214f73fb65
anirudh.r <anirudh.r@flipkart.com>
2016-06-03 19:52:28 +0530

    moved to another package ARA-135

---
+++ flipperf/src/main/java/com/flipkart/flipperf/newlib/reporter/NetworkEventReporter.java
@@ -0,0 +1,60 @@
+package com.flipkart.flipperf.newlib.reporter;
+
+
+import java.io.IOException;
+import java.net.URL;
+
+
+/**
+ *
+ */
+public interface NetworkEventReporter {
+
+    /**
+     * Notifies the {@link NetworkEventReporter} that the intercepted {@link com.squareup.okhttp.Response} headers has been received
+     *
+     * @param inspectorResponse : contains response headers
+     */
+    void responseReceived(InspectorRequest inspectorRequest, InspectorResponse inspectorResponse);
+
+    /**
+     * Reports any {@link IOException} while {@link com.squareup.okhttp.Response} is being proceeded.
+     *
+     * @param inspectorRequest {@link InspectorRequest}
+     * @param e                : error message
+     */
+    void httpExchangeError(InspectorRequest inspectorRequest, IOException e);
+
+    /**
+     * Reports error while getting the input steam from {@link com.squareup.okhttp.ResponseBody}
+     *
+     * @param inspectorResponse {@link InspectorResponse}
+     * @param e                 : error message
+     */
+    void responseInputStreamError(InspectorRequest inspectorRequest, InspectorResponse inspectorResponse, IOException e);
+
+    interface InspectorRequest {
+        int requestId();
+
+        URL url();
+
+        String method();
+
+        long requestSize();
+
+        String hostName();
+    }
+
+    interface InspectorResponse {
+
+        int requestId();
+
+        int statusCode();
+
+        long responseSize();
+
+        long startTime();
+
+        long endTime();
+    }
+}
---
+++ flipperf/src/main/java/com/flipkart/flipperf/newlib/reporter/NetworkEventReporterImpl.java
@@ -0,0 +1,75 @@
+package com.flipkart.flipperf.newlib.reporter;
+
+import com.flipkart.flipperf.newlib.handler.NetworkRequestStatsHandler;
+import com.flipkart.flipperf.newlib.model.RequestStats;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.ResponseBody;
+
+import java.io.IOException;
+
+/**
+ * Created by anirudh.r on 09/05/16 at 12:32 PM.
+ * <p/>
+ * Whenever we receive {@link NetworkEventReporterImpl#responseReceived(InspectorRequest, InspectorResponse)} callback
+ * <p/>
+ * In case of any {@link IOException} during the {@link com.squareup.okhttp.Interceptor.Chain#proceed(Request)},
+ * {@link NetworkEventReporter#httpExchangeError(InspectorRequest, IOException)} gets called with appropriate error message.
+ * <p/>
+ * In case of any {@link IOException} during the {@link ResponseBody#byteStream()}, {@link NetworkEventReporter#responseInputStreamError(InspectorRequest, InspectorResponse, IOException)}
+ * <p/>
+ * <p/>
+ */
+public class NetworkEventReporterImpl implements NetworkEventReporter {
+
+    private NetworkRequestStatsHandler mNetworkRequestStatsHandler;
+
+    public NetworkEventReporterImpl(NetworkRequestStatsHandler networkRequestStatsHandler) {
+        this.mNetworkRequestStatsHandler = networkRequestStatsHandler;
+    }
+
+    @Override
+    public void responseReceived(final InspectorRequest inspectorRequest, final InspectorResponse inspectorResponse) {
+        if (inspectorRequest != null && inspectorRequest != null) {
+            final int requestId = inspectorResponse.requestId();
+            RequestStats requestStats = new RequestStats(requestId);
+            requestStats.setRequestSize(inspectorRequest.requestSize());
+            requestStats.setUrl(inspectorRequest.url());
+            requestStats.setMethodType(inspectorRequest.method());
+            requestStats.setHostName(inspectorRequest.hostName());
+            requestStats.setResponseSize(inspectorResponse.responseSize());
+            requestStats.setStatusCode(inspectorResponse.statusCode());
+            requestStats.setStartTime(inspectorResponse.startTime());
+            requestStats.setEndTime(inspectorResponse.endTime());
+            mNetworkRequestStatsHandler.onResponseReceived(requestStats);
+        }
+    }
+
+    @Override
+    public void httpExchangeError(final InspectorRequest inspectorRequest, final IOException e) {
+        if (inspectorRequest != null) {
+            final int requestId = inspectorRequest.requestId();
+            RequestStats requestStats = new RequestStats(requestId);
+            requestStats.setUrl(inspectorRequest.url());
+            requestStats.setMethodType(inspectorRequest.method());
+            requestStats.setHostName(inspectorRequest.hostName());
+            requestStats.setRequestSize(inspectorRequest.requestSize());
+            mNetworkRequestStatsHandler.onHttpExchangeError(requestStats, e);
+        }
+    }
+
+    @Override
+    public void responseInputStreamError(final InspectorRequest inspectorRequest, final InspectorResponse inspectorResponse, final IOException e) {
+        if (inspectorRequest != null && inspectorResponse != null) {
+            final int requestId = inspectorResponse.requestId();
+            RequestStats requestStats = new RequestStats(requestId);
+            requestStats.setRequestSize(inspectorRequest.requestSize());
+            requestStats.setUrl(inspectorRequest.url());
+            requestStats.setMethodType(inspectorRequest.method());
+            requestStats.setHostName(inspectorRequest.hostName());
+            requestStats.setStatusCode(inspectorResponse.statusCode());
+            requestStats.setStartTime(inspectorResponse.startTime());
+            requestStats.setEndTime(inspectorResponse.endTime());
+            mNetworkRequestStatsHandler.onResponseInputStreamError(requestStats, e);
+        }
+    }
+}
